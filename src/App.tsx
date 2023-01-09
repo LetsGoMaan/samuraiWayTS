c353b73a5b82a05ec9ef3bec84a635c2f24f10c0
Andrey Prokhorov <ilikeseik@gmail.com>
2023-01-09 14:16:51 +0300

    add some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Techologies";
+import {Header} from "./Header";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header/>
+        <Technologies/>
     </div>
   );
 }
+
 
 export default App;
